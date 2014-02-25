8230c6739faf89ec15d80bc6fe6a4177dd5c3829
Valentina Mancinelli <vale.mancy@gmail.com>
2014-02-25 16:01:33 +0100

    fixed bug wrong name resource

--- Lib/CRABTools/CRABServer.py
+++ Lib/CRABTools/CRABServer.py
@@ -242,7 +242,7 @@ class CRABServer(GangaObject):
         """Submit a new task to CRAB3 """
         logger.info('userproxy: %s' % job.backend.userproxy)
         logger.info('server_name: %s' % job.backend.server_name)
-        logger.info('apiurl: %s' % job.backend.apiurl)
+        logger.info('apiresource: %s' % job.backend.apiresource)
         #srv='hammer-crab3.cern.ch' #'cmsweb-testbed.cern.ch'
         #proxypath='/afs/cern.ch/user/r/riahi/public/proxy'#'/afs/cern.ch/user/s/spiga/public/PerValentaina/proxy'
         server = HTTPRequests(job.backend.server_name, job.backend.userproxy)
